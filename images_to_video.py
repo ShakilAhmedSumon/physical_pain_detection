0bec32f7bee492d3fdd8ce924a2b18bc5dc160b2
Shakil Ahmed Sumon <ashakil007@gmail.com>
2021-10-01 00:12:26 +0600

    Update images_to_video.py

--- images_to_video.py
+++ images_to_video.py
@@ -1,25 +1,3 @@
-# import cv2
-# import numpy as np
-# import glob
-
-
-# img_array = []
-# size = 0.0
-
-# for filename in glob.glob('images/*.jpg'):
-#     img = cv2.imread(filename)
-#     height, width, layers = img.shape
-#     size = (width,height)
-#     img_array.append(img)
-
-
-# out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
-
-# for i in range(len(img_array)):
-#     out.write(img_array[i])
-
-# out.release()
-
 import cv2
 import numpy as np
 import os
